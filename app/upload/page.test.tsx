c45796f87f985fffa128a0c78cc42d03a0c727ba
HaradaTakayuki <takayuki.harada@bnt.benextgroup.jp>
2024-02-26 07:51:57 +0000

    アップロードのテストページ更新

--- app/upload/page.test.tsx
+++ app/upload/page.test.tsx
@@ -8,4 +8,7 @@ render(<Upload />)
 test('main test', () => {
   const main = document.querySelector('main')
   expect(main?.getAttribute('class')).toEqual('flex min-h-screen flex-col p-24')
+  expect(main?.textContent).toEqual(
+    'アップロードフォームお名前所属会社社員番号連絡可能な個人電話番号メールアドレス個人情報提供に同意いただけますか?同意する画像をアップロードする確認画面へ',
+  )
 })
