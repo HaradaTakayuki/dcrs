179c4ef55a95dc371768dc25d51e690c96442d69
HaradaTakayuki <takayuki.harada@bnt.benextgroup.jp>
2024-02-26 06:35:33 +0000

    ダウンロードのテストページ更新

--- app/download/page.test.tsx
+++ app/download/page.test.tsx
@@ -61,6 +61,7 @@ test('p test', () => {
 test('div test', () => {
   const div1 = document.querySelectorAll('div')[1]
   const div2 = document.querySelectorAll('div')[2]
+  const div3 = document.querySelectorAll('div')[3]
   const div4 = document.querySelectorAll('div')[4]
   expect(div1?.getAttribute('class')).toEqual(
     'z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex',
@@ -68,7 +69,90 @@ test('div test', () => {
   expect(div2?.getAttribute('class')).toEqual(
     'fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none',
   )
+  expect(div2?.textContent).toEqual('By ')
+  expect(div3?.textContent).toEqual('')
   expect(div4?.getAttribute('class')).toEqual(
     'mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left',
   )
+})
+
+test('a test', () => {
+  const a0 = document.querySelectorAll('a')[0]
+  const a1 = document.querySelectorAll('a')[1]
+  const a2 = document.querySelectorAll('a')[2]
+  const a3 = document.querySelectorAll('a')[3]
+  const a4 = document.querySelectorAll('a')[4]
+  expect(a0?.getAttribute('class')).toEqual(
+    'pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0',
+  )
+  expect(a0?.textContent).toEqual('By ')
+  expect(a0?.getAttribute('href')).toEqual(
+    'https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+  )
+  expect(a0?.getAttribute('target')).toEqual('_blank')
+  expect(a0?.getAttribute('rel')).toEqual('noopener noreferrer')
+  expect(a1?.getAttribute('class')).toEqual(
+    'group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30',
+  )
+  expect(a1?.textContent).toEqual(
+    'Docs ->Find in-depth information about Next.js features and API.',
+  )
+  expect(a1?.getAttribute('href')).toEqual(
+    'https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+  )
+  expect(a1?.getAttribute('target')).toEqual('_blank')
+  expect(a1?.getAttribute('rel')).toEqual('noopener noreferrer')
+  expect(a2?.getAttribute('class')).toEqual(
+    'group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30',
+  )
+  expect(a2?.getAttribute('href')).toEqual(
+    'https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app',
+  )
+  expect(a2?.getAttribute('target')).toEqual('_blank')
+  expect(a2?.getAttribute('rel')).toEqual('noopener noreferrer')
+  expect(a3?.getAttribute('class')).toEqual(
+    'group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30',
+  )
+  expect(a3?.textContent).toEqual(
+    'Templates ->Explore the Next.js 13 playground.',
+  )
+  expect(a3?.getAttribute('href')).toEqual(
+    'https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+  )
+  expect(a3?.getAttribute('target')).toEqual('_blank')
+  expect(a3?.getAttribute('rel')).toEqual('noopener noreferrer')
+  expect(a4?.getAttribute('class')).toEqual(
+    'group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30',
+  )
+  expect(a4?.textContent).toEqual(
+    'Deploy ->Instantly deploy your Next.js site to a shareable URL with Vercel.',
+  )
+  expect(a4?.getAttribute('href')).toEqual(
+    'https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app',
+  )
+  expect(a4?.getAttribute('target')).toEqual('_blank')
+  expect(a4?.getAttribute('rel')).toEqual('noopener noreferrer')
+})
+
+test('span test', () => {
+  const span0 = document.querySelectorAll('span')[0]
+  const span1 = document.querySelectorAll('span')[1]
+  const span2 = document.querySelectorAll('span')[2]
+  const span3 = document.querySelectorAll('span')[3]
+  expect(span0?.getAttribute('class')).toEqual(
+    'inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none',
+  )
+  expect(span0?.textContent).toEqual('->')
+  expect(span1?.getAttribute('class')).toEqual(
+    'inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none',
+  )
+  expect(span1?.textContent).toEqual('->')
+  expect(span2?.getAttribute('class')).toEqual(
+    'inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none',
+  )
+  expect(span2?.textContent).toEqual('->')
+  expect(span3?.getAttribute('class')).toEqual(
+    'inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none',
+  )
+  expect(span3?.textContent).toEqual('->')
 })
